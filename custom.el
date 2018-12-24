6b5a843c675237c93c5de06b584d14ea9d2ba9fb
Benj Bellon <benjaminbellon@gmail.com>
2018-12-24 09:51:47 -0800

    basic customization separated

---
+++ custom.el
@@ -0,0 +1,7 @@
+(require 'setup-custom)
+;; reserved for all local customization added by emacs
+;; do not modify
+(custom-set-variables)
+(custom-set-faces)
+(provide 'custom)
+;;; custom.el ends here
